92eb27b06b19738f31da89ba30f9592436dd6e19
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2021-12-11 19:02:06 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDE45e0OphdZuX6AW9Z7SspnW-L9gmtKkQ",
+  authDomain: "lightning-chat-22e93.firebaseapp.com",
+  projectId: "lightning-chat-22e93",
+  storageBucket: "lightning-chat-22e93.appspot.com",
+  messagingSenderId: "864319874978",
+  appId: "1:864319874978:web:9d445ab26e4d250d25b4fb"
+};
+
+const app = initializeApp(firebaseConfig);
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+     Room_names = childKey;
+    
+    });});}
+getData();
