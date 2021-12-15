0a73866ac0d8c1a1a057506d2a696112741a29e7
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2021-12-15 16:56:30 -0600

    Update Kwitter.js

--- Kwitter.js
+++ Kwitter.js
@@ -13,8 +13,8 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 function addUser(){
     user_name = document.getElementById("user_name").value;
-    //localStorage.setItem("user_name" ,user_name );
-    //window.location="kwitter_room.html";
+    localStorage.setItem("user_name" ,user_name );
+    window.location="kwitter_room.html";
     firebase.database().ref("/").child(user_name).update({
         purpose: "add user"
     })
